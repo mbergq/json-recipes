626a9f0a92b03d70330bbb0b09e7d57f7ba9741f
Martin <106612709+menkodot@users.noreply.github.com>
2024-08-14 10:19:55 +0200

    update recipe data

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// console logs to test with node
+
 let data = require("./recipes.json");
 // console.log(JSON.stringify(data));
 // console.log(data.recipes);
@@ -14,6 +16,18 @@ let data = require("./recipes.json");
 // console.log(data.recipes[0].language[0].english);
 // console.log(data.recipes[1].language[0].swedish);
 
-data.recipes[1].language[0].swedish.ingredients.map((i) => {
+data.recipes[0].language[0].swedish.ingredients.map((i) => {
   console.log(i.name + " " + i.amount);
 });
+
+console.log(data.recipes[0].language[0].swedish.instructions);
+
+// fetch("https://mbergq.github.io/json-recipes/path/to/your/jsonfile.json")'
+
+async function fetchData() {
+  const res = await fetch("https://mbergq.github.io/json-recipes/recipes.json");
+  const data = await res.json();
+  console.log(data);
+}
+
+fetchData();
