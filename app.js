742e69899228c66f5d42ec9ee29c15953810763e
Martin <106612709+menkodot@users.noreply.github.com>
2024-08-15 09:23:47 +0200

    add id's to recipes

--- app.js
+++ app.js
@@ -15,19 +15,21 @@ let data = require("./recipes.json");
 // console.log(data);
 // console.log(data.recipes[0].language[0].english);
 // console.log(data.recipes[1].language[0].swedish);
-
+// const testPath = data.recipes[0].language[0].swedish.ingredients;
+// console.log(testPath);
 data.recipes[0].language[0].swedish.ingredients.map((i) => {
   console.log(i.name + " " + i.amount);
 });
 
-console.log(data.recipes[0].language[0].swedish.instructions);
+// console.log(data.recipes[0].language[0].swedish.instructions);
 
 // fetch("https://mbergq.github.io/json-recipes/path/to/your/jsonfile.json")'
 
 async function fetchData() {
   const res = await fetch("https://mbergq.github.io/json-recipes/recipes.json");
   const data = await res.json();
-  console.log(data);
+  console.log(data.recipes[0].language[0].swedish);
+  console.log(data.recipes[0].language[0].swedish.name);
 }
 
 fetchData();
